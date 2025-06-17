zerorisc/expo

[sca] Add SPI stubbing parameter
This is useful to save resource utilization on smaller FPGA boards
and easier than to omit the full SPI host instance and disentangle
with pinmux.

Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

hw/ip/spi_host/rtl/spi_host_window.sv
@@ -7,6 +7,7 @@
 
 module spi_host_window
 #(
+  parameter bit                             Stub                   = 1'b0,
   parameter bit                             EnableRacl             = 1'b0,
   parameter bit                             RaclErrorRsp           = 1'b1,
   parameter top_racl_pkg::racl_policy_sel_t RaclPolicySelWinRXDATA = 0,
@@ -40,85 +41,112 @@ module spi_host_window
     }
   };
 
-  logic         rx_we;
+  if (Stub) begin : gen_stubbed_memory
+    // Stub this window off with error responders if stubbed.
+    tlul_err_resp u_tlul_err_resp_rx (
+      .clk_i,
+      .rst_ni,
+      .tl_h_i(rx_win_i),
+      .tl_h_o(rx_win_o)
+    );
+    tlul_err_resp u_tlul_err_resp_tx (
+      .clk_i,
+      .rst_ni,
+      .tl_h_i(tx_win_i),
+      .tl_h_o(tx_win_o)
+    );
 
-  // Only support reads from the data RX fifo window
-  logic  rx_access_error;
-  assign rx_access_error = rx_we;
+    // Tie off unused signals
+    assign rx_ready_o  = '0;
+    assign tx_valid_o  = '0;
+    assign tx_data_o   = '0;
+    assign bit_mask    = '0;
 
-  tlul_adapter_reg_racl #(
-    .RegAw             ( AW                       ),
-    .RegDw             ( DW                       ),
-    .EnableDataIntgGen ( 0                        ),
-    .EnableRacl        ( EnableRacl               ),
-    .RaclErrorRsp      ( RaclErrorRsp             ),
-    .RaclPolicySelVec  ( RaclPolicySelWinRXDATA   )
-  ) u_adapter_rx (
-    .clk_i,
-    .rst_ni,
-    .tl_i             ( rx_win_i                  ),
-    .tl_o             ( rx_win_o                  ),
-    .en_ifetch_i      ( prim_mubi_pkg::MuBi4False ),
-    .intg_error_o     (                           ),
-    .racl_policies_i  ( racl_policies_i           ),
-    .racl_error_o     ( racl_error_rx_o           ),
-    .we_o             ( rx_we                     ),
-    .re_o             ( rx_ready_o                ),
-    .addr_o           (                           ),
-    .wdata_o          (                           ),
-    .be_o             (                           ),
-    .rdata_i          ( rx_data_i                 ),
-    .error_i          ( rx_access_error           ),
-    .busy_i           ( '0                        )
-  );
+    logic unused_sigs;
+    assign unused_sigs = ^{
+      rx_data_i
+    };
+  end else begin : gen_no_stubbed_memory
+    logic         rx_we;
 
-  // translate bitmask to byte mask
-  logic [DW-1:0] bit_mask;
-  for (genvar i = 0; i < ByteMaskW; i++) begin : gen_byte_mask
-     assign tx_be_o[i] = |bit_mask[i*8 +: 8];
+    // Only support reads from the data RX fifo window
+    logic  rx_access_error;
+    assign rx_access_error = rx_we;
 
-    // all the bits of particular byte must be the same
-    `ASSERT(BitMaskCheck_A, (|bit_mask[i*8 +: 8] == 1'b0) ||
-                            (&bit_mask[i*8 +: 8] == 1'b1))
+    tlul_adapter_reg_racl #(
+      .RegAw             ( AW                       ),
+      .RegDw             ( DW                       ),
+      .EnableDataIntgGen ( 0                        ),
+      .EnableRacl        ( EnableRacl               ),
+      .RaclErrorRsp      ( RaclErrorRsp             ),
+      .RaclPolicySelVec  ( RaclPolicySelWinRXDATA   )
+    ) u_adapter_rx (
+      .clk_i,
+      .rst_ni,
+      .tl_i             ( rx_win_i                  ),
+      .tl_o             ( rx_win_o                  ),
+      .en_ifetch_i      ( prim_mubi_pkg::MuBi4False ),
+      .intg_error_o     (                           ),
+      .racl_policies_i  ( racl_policies_i           ),
+      .racl_error_o     ( racl_error_rx_o           ),
+      .we_o             ( rx_we                     ),
+      .re_o             ( rx_ready_o                ),
+      .addr_o           (                           ),
+      .wdata_o          (                           ),
+      .be_o             (                           ),
+      .rdata_i          ( rx_data_i                 ),
+      .error_i          ( rx_access_error           ),
+      .busy_i           ( '0                        )
+    );
+
+    // translate bitmask to byte mask
+    logic [DW-1:0] bit_mask;
+    for (genvar i = 0; i < ByteMaskW; i++) begin : gen_byte_mask
+      assign tx_be_o[i] = |bit_mask[i*8 +: 8];
+
+      // all the bits of particular byte must be the same
+      `ASSERT(BitMaskCheck_A, (|bit_mask[i*8 +: 8] == 1'b0) ||
+                              (&bit_mask[i*8 +: 8] == 1'b1))
+    end
+
+    // Only support writes to the data TX fifo window
+    tlul_adapter_sram_racl #(
+      .SramAw(AW),
+      .SramDw(DW),
+      .Outstanding(1),
+      .ByteAccess(1),
+      .ErrOnWrite(0),
+      .ErrOnRead(1),
+      .EnableRacl(EnableRacl),
+      .RaclErrorRsp(RaclErrorRsp),
+      .RaclPolicySelNumRanges(1),
+      .RaclPolicySelRanges(RaclPolicySelRangesTXDATA)
+    ) u_adapter_tx (
+      .clk_i,
+      .rst_ni,
+      .tl_i(tx_win_i),
+      .tl_o(tx_win_o),
+      .en_ifetch_i(prim_mubi_pkg::MuBi4False),
+      .req_o(tx_valid_o),
+      .req_type_o(),
+      .gnt_i(1'b1),
+      .we_o(),
+      .addr_o(),
+      .wdata_o(tx_data_o),
+      .wmask_o(bit_mask),
+      .intg_error_o(),
+      .user_rsvd_o(),
+      .rdata_i('0),
+      .rvalid_i('0),
+      .rerror_i('0),
+      .compound_txn_in_progress_o(),
+      .readback_en_i(prim_mubi_pkg::MuBi4False),
+      .readback_error_o (),
+      .wr_collision_i(1'b0),
+      .write_pending_i(1'b0),
+      .racl_policies_i  (racl_policies_i),
+      .racl_error_o     (racl_error_tx_o)
+    );
   end
-
-  // Only support writes to the data TX fifo window
-  tlul_adapter_sram_racl #(
-    .SramAw(AW),
-    .SramDw(DW),
-    .Outstanding(1),
-    .ByteAccess(1),
-    .ErrOnWrite(0),
-    .ErrOnRead(1),
-    .EnableRacl(EnableRacl),
-    .RaclErrorRsp(RaclErrorRsp),
-    .RaclPolicySelNumRanges(1),
-    .RaclPolicySelRanges(RaclPolicySelRangesTXDATA)
-  ) u_adapter_tx (
-    .clk_i,
-    .rst_ni,
-    .tl_i(tx_win_i),
-    .tl_o(tx_win_o),
-    .en_ifetch_i(prim_mubi_pkg::MuBi4False),
-    .req_o(tx_valid_o),
-    .req_type_o(),
-    .gnt_i(1'b1),
-    .we_o(),
-    .addr_o(),
-    .wdata_o(tx_data_o),
-    .wmask_o(bit_mask),
-    .intg_error_o(),
-    .user_rsvd_o(),
-    .rdata_i('0),
-    .rvalid_i('0),
-    .rerror_i('0),
-    .compound_txn_in_progress_o(),
-    .readback_en_i(prim_mubi_pkg::MuBi4False),
-    .readback_error_o (),
-    .wr_collision_i(1'b0),
-    .write_pending_i(1'b0),
-    .racl_policies_i  (racl_policies_i),
-    .racl_error_o     (racl_error_tx_o)
-  );
 
 endmodule : spi_host_window

ziuziakowska/opentitan

hw/ip/spi_host/rtl/spi_host_window.sv
@@ -7,6 +7,7 @@
 
 module spi_host_window
 #(
+  parameter bit                             Stub                   = 1'b0,
   parameter bit                             EnableRacl             = 1'b0,
   parameter bit                             RaclErrorRsp           = 1'b1,
   parameter top_racl_pkg::racl_policy_sel_t RaclPolicySelWinRXDATA = 0,
@@ -41,85 +42,112 @@ module spi_host_window
     }
   };
 
-  logic         rx_we;
+  if (Stub) begin : gen_stubbed_memory
+    // Stub this window off with error responders if stubbed.
+    tlul_err_resp u_tlul_err_resp_rx (
+      .clk_i,
+      .rst_ni,
+      .tl_h_i(rx_win_i),
+      .tl_h_o(rx_win_o)
+    );
+    tlul_err_resp u_tlul_err_resp_tx (
+      .clk_i,
+      .rst_ni,
+      .tl_h_i(tx_win_i),
+      .tl_h_o(tx_win_o)
+    );
 
-  // Only support reads from the data RX fifo window
-  logic  rx_access_error;
-  assign rx_access_error = rx_we;
+    // Tie off unused signals
+    assign rx_ready_o  = '0;
+    assign tx_valid_o  = '0;
+    assign tx_data_o   = '0;
+    assign bit_mask    = '0;
 
-  tlul_adapter_reg_racl #(
-    .RegAw             ( AW                       ),
-    .RegDw             ( DW                       ),
-    .EnableDataIntgGen ( 0                        ),
-    .EnableRacl        ( EnableRacl               ),
-    .RaclErrorRsp      ( RaclErrorRsp             ),
-    .RaclPolicySelVec  ( RaclPolicySelWinRXDATA   )
-  ) u_adapter_rx (
-    .clk_i,
-    .rst_ni,
-    .tl_i             ( rx_win_i                  ),
-    .tl_o             ( rx_win_o                  ),
-    .en_ifetch_i      ( prim_mubi_pkg::MuBi4False ),
-    .intg_error_o     (                           ),
-    .racl_policies_i  ( racl_policies_i           ),
-    .racl_error_o     ( racl_error_rx_o           ),
-    .we_o             ( rx_we                     ),
-    .re_o             ( rx_ready_o                ),
-    .addr_o           (                           ),
-    .wdata_o          (                           ),
-    .be_o             (                           ),
-    .rdata_i          ( rx_data_i                 ),
-    .error_i          ( rx_access_error           ),
-    .busy_i           ( '0                        )
-  );
+    logic unused_sigs;
+    assign unused_sigs = ^{
+      rx_data_i
+    };
+  end else begin : gen_no_stubbed_memory
+    logic         rx_we;
 
-  // translate bitmask to byte mask
-  logic [DW-1:0] bit_mask;
-  for (genvar i = 0; i < ByteMaskW; i++) begin : gen_byte_mask
-     assign tx_be_o[i] = |bit_mask[i*8 +: 8];
+    // Only support reads from the data RX fifo window
+    logic  rx_access_error;
+    assign rx_access_error = rx_we;
 
-    // all the bits of particular byte must be the same
-    `ASSERT(BitMaskCheck_A, (|bit_mask[i*8 +: 8] == 1'b0) ||
-                            (&bit_mask[i*8 +: 8] == 1'b1))
+    tlul_adapter_reg_racl #(
+      .RegAw             ( AW                       ),
+      .RegDw             ( DW                       ),
+      .EnableDataIntgGen ( 0                        ),
+      .EnableRacl        ( EnableRacl               ),
+      .RaclErrorRsp      ( RaclErrorRsp             ),
+      .RaclPolicySelVec  ( RaclPolicySelWinRXDATA   )
+    ) u_adapter_rx (
+      .clk_i,
+      .rst_ni,
+      .tl_i             ( rx_win_i                  ),
+      .tl_o             ( rx_win_o                  ),
+      .en_ifetch_i      ( prim_mubi_pkg::MuBi4False ),
+      .intg_error_o     (                           ),
+      .racl_policies_i  ( racl_policies_i           ),
+      .racl_error_o     ( racl_error_rx_o           ),
+      .we_o             ( rx_we                     ),
+      .re_o             ( rx_ready_o                ),
+      .addr_o           (                           ),
+      .wdata_o          (                           ),
+      .be_o             (                           ),
+      .rdata_i          ( rx_data_i                 ),
+      .error_i          ( rx_access_error           ),
+      .busy_i           ( '0                        )
+    );
+
+    // translate bitmask to byte mask
+    logic [DW-1:0] bit_mask;
+    for (genvar i = 0; i < ByteMaskW; i++) begin : gen_byte_mask
+      assign tx_be_o[i] = |bit_mask[i*8 +: 8];
+
+      // all the bits of particular byte must be the same
+      `ASSERT(BitMaskCheck_A, (|bit_mask[i*8 +: 8] == 1'b0) ||
+                              (&bit_mask[i*8 +: 8] == 1'b1))
+    end
+
+    // Only support writes to the data TX fifo window
+    tlul_adapter_sram_racl #(
+      .SramAw(AW),
+      .SramDw(DW),
+      .Outstanding(1),
+      .ByteAccess(1),
+      .ErrOnWrite(0),
+      .ErrOnRead(1),
+      .EnableRacl(EnableRacl),
+      .RaclErrorRsp(RaclErrorRsp),
+      .RaclPolicySelNumRanges(1)
+    ) u_adapter_tx (
+      .clk_i,
+      .rst_ni,
+      .tl_i(tx_win_i),
+      .tl_o(tx_win_o),
+      .en_ifetch_i(prim_mubi_pkg::MuBi4False),
+      .req_o(tx_valid_o),
+      .req_type_o(),
+      .gnt_i(1'b1),
+      .we_o(),
+      .addr_o(),
+      .wdata_o(tx_data_o),
+      .wmask_o(bit_mask),
+      .intg_error_o(),
+      .user_rsvd_o(),
+      .rdata_i('0),
+      .rvalid_i('0),
+      .rerror_i('0),
+      .compound_txn_in_progress_o(),
+      .readback_en_i(prim_mubi_pkg::MuBi4False),
+      .readback_error_o (),
+      .wr_collision_i(1'b0),
+      .write_pending_i(1'b0),
+      .racl_policies_i  (racl_policies_i),
+      .racl_error_o     (racl_error_tx_o),
+      .racl_policy_sel_ranges_i(RaclPolicySelRangesTXDATA)
+    );
   end
-
-  // Only support writes to the data TX fifo window
-  tlul_adapter_sram_racl #(
-    .SramAw(AW),
-    .SramDw(DW),
-    .Outstanding(1),
-    .ByteAccess(1),
-    .ErrOnWrite(0),
-    .ErrOnRead(1),
-    .EnableRacl(EnableRacl),
-    .RaclErrorRsp(RaclErrorRsp),
-    .RaclPolicySelNumRanges(1)
-  ) u_adapter_tx (
-    .clk_i,
-    .rst_ni,
-    .tl_i(tx_win_i),
-    .tl_o(tx_win_o),
-    .en_ifetch_i(prim_mubi_pkg::MuBi4False),
-    .req_o(tx_valid_o),
-    .req_type_o(),
-    .gnt_i(1'b1),
-    .we_o(),
-    .addr_o(),
-    .wdata_o(tx_data_o),
-    .wmask_o(bit_mask),
-    .intg_error_o(),
-    .user_rsvd_o(),
-    .rdata_i('0),
-    .rvalid_i('0),
-    .rerror_i('0),
-    .compound_txn_in_progress_o(),
-    .readback_en_i(prim_mubi_pkg::MuBi4False),
-    .readback_error_o (),
-    .wr_collision_i(1'b0),
-    .write_pending_i(1'b0),
-    .racl_policies_i  (racl_policies_i),
-    .racl_error_o     (racl_error_tx_o),
-    .racl_policy_sel_ranges_i(RaclPolicySelRangesTXDATA)
-  );
 
 endmodule : spi_host_window